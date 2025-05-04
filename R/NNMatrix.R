e517a8089c2abab7c1efad8837210c69428517b6
Mahlon Scott <1kingjms@gmail.com>
2025-05-04 15:18:54 -0400

    Remove unused functions from NNMatrix

--- R/NNMatrix.R
+++ R/NNMatrix.R
@@ -3,8 +3,6 @@ i_dist <- function(i, neighbor_index, s) {
     dist(s[c(i, neighbor_index[[i - 1]]), ])
 }
 
-ho_dist <- function(i, nn.ind, coords) c(dist(coords[nn.ind[i, ], ]))
-
 get_NN_distM <- function(ind, ind_distM_d, M) {
     if (ind < M) {
         l <- ind
@@ -66,33 +64,4 @@ NNMatrix <- function(coords, n.neighbors, n.omp.threads = 2,
         ord = ord, coords.ord = coords.ord,
         NN_ind = NN_ind, NN_distM = NN_distM, NN_dist = NN_dist
     ))
-}
-
-#### Function for checking neighbors ####
-
-Check_Neighbors <- function(coords.ord, n.neighbors, NN.matrix, ind) {
-    plot(coords.ord)
-    points(coords.ord[1:ind, , drop = FALSE], col = "grey", pch = 19)
-
-    # neighbors
-    if (ind < n.neighbors) {
-        dim <- ind
-    } else {
-        dim <- n.neighbors
-    }
-    for (j in 1:dim) {
-        points(coords.ord[NN.matrix$NN_ind[ind - 1, j], , drop = FALSE],
-            col = "orange", pch = 19
-        )
-    }
-    points(coords.ord[ind, , drop = FALSE], col = "blue", pch = 19)
-    legend("topleft",
-        inset = .05,
-        c(
-            "obs", paste0(ind, "th obs"),
-            paste0("nb. of ", ind, "th obs"),
-            paste0("obs <", ind)
-        ), pch = c(1, 19, 19, 19),
-        col = c("black", "blue", "orange", "grey"), horiz = F
-    )
 }
